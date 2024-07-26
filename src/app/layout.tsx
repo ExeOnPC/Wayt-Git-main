e37252cb3e22bf22899ca806bafc96b8c8dffe94
ExeOnPC <Jakepoakley@gmail.com>
2024-07-26 18:27:49 +1000

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   README.md 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.mjs 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   src/app/favicon.ico 	new file:   src/app/globals.css 	new file:   src/app/layout.tsx 	new file:   src/app/page.tsx 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wayt",
+  description: "What's Around Your Town?",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
